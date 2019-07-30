4535af33d946dba7a2e1d6baa1ea09aa20b1e862
Elias Carvalho <eliascarvalho01@hotmail.com>
2019-07-30 20:55:03 -0300

    Bishop

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -10,6 +10,11 @@ public class Pawn extends ChessPiece{
 	public Pawn(Board board, Color color) {
 		super(board, color);
 	}
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
@@ -43,7 +48,7 @@ public class Pawn extends ChessPiece{
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow() + 2, position.getColumn());
-			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && 
 				getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
@@ -58,10 +63,5 @@ public class Pawn extends ChessPiece{
 			}
 		}		
 		return mat;
-	}
-	
-	@Override
-	public String toString() {
-		return "P";
 	}
 }
